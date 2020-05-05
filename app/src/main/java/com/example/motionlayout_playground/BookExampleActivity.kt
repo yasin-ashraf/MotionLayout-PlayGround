30e1a08e167c531b13e1aee2b40d89eac84dcd0d
Yasin Ashraf <yasin.asharf@gmail.com>
2020-05-06 02:25:04 +0530

    motion 2

---
+++ app/src/main/java/com/example/motionlayout_playground/BookExampleActivity.kt
@@ -0,0 +1,16 @@
+package com.example.motionlayout_playground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Yasin on 4/5/20.
+ */
+
+class BookExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book)
+    }
+}
